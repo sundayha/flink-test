d9601605c06b7302a7976ca0a3107633792b8255
张博 <llkjzhangbo@bitbucket.org>
2019-02-24 11:32:22 +0800

    实现 sql 查询

--- src/main/java/readcsv/ReadCSV.java
+++ src/main/java/readcsv/ReadCSV.java
@@ -6,6 +6,10 @@ import org.apache.flink.api.java.io.PojoCsvInputFormat;
 import org.apache.flink.api.java.typeutils.PojoTypeInfo;
 import org.apache.flink.api.java.typeutils.TypeExtractor;
 import org.apache.flink.core.fs.Path;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.java.BatchTableEnvironment;
+
+import java.util.List;
 
 /**
  * @author 张博【zhangb@lianliantech.cn】
@@ -14,17 +18,32 @@ public class ReadCSV {
 
     public static void main(String[] args) throws Exception {
         ExecutionEnvironment see = ExecutionEnvironment.getExecutionEnvironment();
-        //DataSet<WaferHead> dataSet = see.readCsvFile("/Users/zhangbo/Downloads/ND_SVD_DEV_wafer_aoi_head.csv").pojoType(WaferHead.class);
+
+        BatchTableEnvironment tableEnvironment = BatchTableEnvironment.getTableEnvironment(see);
         PojoTypeInfo<WaferHead> pojoType = (PojoTypeInfo<WaferHead>) TypeExtractor.createTypeInfo(WaferHead.class);
         // 由于 Java 反射抽取出的字段顺序是不确定的，需要显式指定下文件中字段的顺序
         String[] fieldOrder = new String[]{"id", "waferId", "path", "state"};
         // 创建 PojoCsvInputFormat
         PojoCsvInputFormat<WaferHead> csvInput = new PojoCsvInputFormat<>(new Path("/Users/zhangbo/Downloads/ND_SVD_DEV_wafer_aoi_head.csv"), pojoType, fieldOrder);
-
         DataSet<WaferHead> dataSet = see.createInput(csvInput, pojoType);
-
-
-        dataSet.print();
-        see.execute();
+        Table table = tableEnvironment.fromDataSet(dataSet);
+        tableEnvironment.registerTable("waferHead", table);
+        Table sql = tableEnvironment.sqlQuery("select * from waferHead where id in (1, 2)");
+        DataSet<WaferHead> result = tableEnvironment.toDataSet(sql, WaferHead.class);
+        //result.map(new MapFunction<WaferHead, Tuple4<String, String, String, String>>() {
+        //
+        //    @Override
+        //    public Tuple4<String, String, String, String> map(WaferHead value) throws Exception {
+        //        System.out.println(value.getId());
+        //        System.out.println(value.getWaferId());
+        //        return new Tuple4<>(value.getId(), value.getWaferId(), value.getPath(), value.getState());
+        //    }
+        //});
+        List<WaferHead> list =  result.collect();
+        list.forEach(s -> {
+            System.out.println(s.getId());
+            System.out.println(s.getWaferId());
+        });
+        //see.execute();
     }
 }
