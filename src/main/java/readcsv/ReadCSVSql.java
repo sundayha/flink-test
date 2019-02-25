1079b214e3c17700d8894da151b400c988963d54
sundayha <sagapr@163.com>
2019-02-25 17:47:30 +0800

    行读取 csv

--- src/main/java/readcsv/ReadCSVSql.java
+++ src/main/java/readcsv/ReadCSVSql.java
@@ -13,22 +13,32 @@ import java.util.List;
 
 /**
  * @author 张博【zhangb@lianliantech.cn】
+ *
+ * 加载 csv 文件并使用 sql 查询结果
  */
-public class ReadCSV {
+public class ReadCSVSql {
 
     public static void main(String[] args) throws Exception {
-        ExecutionEnvironment see = ExecutionEnvironment.getExecutionEnvironment();
 
+        // 创建执行环境
+        ExecutionEnvironment see = ExecutionEnvironment.getExecutionEnvironment();
+        // 创建表环境
         BatchTableEnvironment tableEnvironment = BatchTableEnvironment.getTableEnvironment(see);
+        // 定义 pojo 的类型
         PojoTypeInfo<WaferHead> pojoType = (PojoTypeInfo<WaferHead>) TypeExtractor.createTypeInfo(WaferHead.class);
         // 由于 Java 反射抽取出的字段顺序是不确定的，需要显式指定下文件中字段的顺序
         String[] fieldOrder = new String[]{"id", "waferId", "path", "state"};
-        // 创建 PojoCsvInputFormat
+        // 创建 PojoCsvInputFormat，读取 csv
         PojoCsvInputFormat<WaferHead> csvInput = new PojoCsvInputFormat<>(new Path("/Users/zhangbo/Downloads/ND_SVD_DEV_wafer_aoi_head.csv"), pojoType, fieldOrder);
+        // 创建 dataSet
         DataSet<WaferHead> dataSet = see.createInput(csvInput, pojoType);
+        // 把 dataSet 转成 table
         Table table = tableEnvironment.fromDataSet(dataSet);
+        // 注册该 table
         tableEnvironment.registerTable("waferHead", table);
+        // 查询该 table
         Table sql = tableEnvironment.sqlQuery("select * from waferHead where id in (1, 2)");
+        // 把查询结果转成 dataSet
         DataSet<WaferHead> result = tableEnvironment.toDataSet(sql, WaferHead.class);
         //result.map(new MapFunction<WaferHead, Tuple4<String, String, String, String>>() {
         //
---
+++ src/main/java/readcsv/ReadCSVline.java
@@ -0,0 +1,23 @@
+package readcsv;
+
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+
+import java.util.List;
+
+/**
+ * @author 张博【zhangb@lianliantech.cn】
+ */
+public class ReadCSVline {
+
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment environment = ExecutionEnvironment.getExecutionEnvironment();
+        DataSet<String> stringDataSet = environment.readTextFile("/Users/zhangbo/Downloads/ND_SVD_DEV_wafer_aoi_head.csv");
+        List<String> aa = stringDataSet.filter(s -> s.startsWith("5")).collect();
+        List<String> strings = stringDataSet.collect();
+        strings.forEach(System.out::println);
+        System.out.println(strings.size());
+        aa.forEach(System.out::println);
+        System.out.println(aa.size());
+    }
+}
