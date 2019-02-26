16f454e23116268a0fc880e9772e835c35c7c1ab
sundayha <sagapr@163.com>
2019-02-26 11:20:25 +0800

    添加 returns。增加注释

--- src/main/java/wordcount/WordCount.java
+++ src/main/java/wordcount/WordCount.java
@@ -1,6 +1,7 @@
 package wordcount;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -8,6 +9,8 @@ import org.apache.flink.streaming.api.windowing.time.Time;
 
 /**
  * @author 张博【zhangb@lianliantech.cn】
+ *
+ * 控制台输入 nc -lk 9000。然后启动程序
  */
 public class WordCount {
     public static void main(String[] args) throws Exception {
@@ -17,11 +20,14 @@ public class WordCount {
         DataStream<String> text = see.socketTextStream("localhost", 9000);
 
         DataStream<Tuple2<String, Integer>> wordCount = text.flatMap((FlatMapFunction<String, Tuple2<String, Integer>>) (value, out) -> {
-            System.out.println("输入字符串" + value);
+            System.out.println("输入字符串：" + value);
             for (String word : value.split(" ")) {
                 out.collect(Tuple2.of(word, 1));
             }
-        }).keyBy(0).timeWindow(Time.seconds(5)).sum(1);
+        }).returns(Types.TUPLE(Types.STRING, Types.INT))
+                .keyBy(0)
+                .timeWindow(Time.seconds(5))
+                .sum(1);
 
         wordCount.print();
         see.execute();
