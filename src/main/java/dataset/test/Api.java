716f30ad77e97da347ba1b35e4ffe0254363400c
sundayha <sagapr@163.com>
2019-03-11 16:52:00 +0800

    flink 学习 测试

--- src/main/java/dataset/test/Api.java
+++ src/main/java/dataset/test/Api.java
@@ -8,6 +8,9 @@ import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.typeutils.PojoTypeInfo;
 import org.apache.flink.api.java.typeutils.TypeExtractor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author 张博【zhangb@lianliantech.cn】
  */
@@ -18,13 +21,17 @@ public class Api {
         environment.setParallelism(3);
 
         PojoTypeInfo<Rating> pojoTypeInfo = (PojoTypeInfo<Rating>) TypeExtractor.createTypeInfo(Rating.class);
-
-        environment.createInput(, pojoTypeInfo);
-        DataSet<Rating> ratings = environment.fromElements(new Rating("李雷", "男", 50), new Rating("韩梅梅", "女", 10), new Rating("郭靖", "男", 100));
+        List<Rating> ratings = new ArrayList<>(3);
+        ratings.add(new Rating("李雷", "男", 50));
+        ratings.add(new Rating("韩梅梅", "女", 10));
+        ratings.add(new Rating("郭靖", "男", 50));
+        DataSet<Rating> ratingss  = environment.fromCollection(ratings, pojoTypeInfo);
+        //environment.createInput(, pojoTypeInfo);
+        //DataSet<Rating> ratings = environment.fromElements(Rating.class, new Rating("李雷", "男", 50), new Rating("", "女", 10), new Rating("郭靖", "男", 100));
 
         DataSet<Tuple2<String, Double>> weights = environment.fromElements(new Tuple2<>("男", 1.0), new Tuple2<>("女", 2.0));
 
-        joinFunctionT(ratings, weights);
+        joinFunctionT(ratingss, weights);
     }
 
     public static void joinFunctionT(DataSet<Rating> ratings, DataSet<Tuple2<String, Double>> weights) throws Exception {
