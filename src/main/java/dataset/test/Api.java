02125ceec17d8210516a87f5f0a6c5a3e259fba2
张博 <llkjzhangbo@bitbucket.org>
2019-03-11 21:08:48 +0800

    测试

--- src/main/java/dataset/test/Api.java
+++ src/main/java/dataset/test/Api.java
@@ -4,11 +4,9 @@ import dataset.test.model.Rating;
 import org.apache.flink.api.common.functions.JoinFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.api.java.io.CollectionInputFormat;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.typeutils.PojoTypeInfo;
 import org.apache.flink.api.java.typeutils.TypeExtractor;
-import org.apache.flink.runtime.state.ArrayListSerializer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,17 +22,23 @@ public class Api {
 
         PojoTypeInfo<Rating> pojoTypeInfo = (PojoTypeInfo<Rating>) TypeExtractor.createTypeInfo(Rating.class);
         List<Rating> ratings = new ArrayList<>(3);
-        ratings.add(new Rating("李雷", "男", 50));
-        ratings.add(new Rating("韩梅梅", "女", 10));
-        ratings.add(new Rating("郭靖", "男", 50));
+        Rating rating = new Rating();
+        Rating rating1 = new Rating();
+        Rating rating2 = new Rating();
+        rating.setName("李雷");
+        rating1.setName("韩梅梅");
+        rating2.setName("郭靖");
+        rating.setCategory("男");
+        rating1.setCategory("女");
+        rating2.setCategory("男");
+        rating.setPoints(50);
+        rating1.setPoints(10);
+        rating2.setPoints(100);
+        ratings.add(rating);
+        ratings.add(rating1);
+        ratings.add(rating2);
 
-        ArrayListSerializer<Rating> arrayListSerializer = new ArrayListSerializer<>();
-
-        CollectionInputFormat<Rating> collectionInputFormat = new CollectionInputFormat<>(ratings, arrayListSerializer);
         DataSet<Rating> ratingss  = environment.fromCollection(ratings, pojoTypeInfo);
-
-        //environment.createInput(, pojoTypeInfo);
-        //DataSet<Rating> ratings = environment.fromElements(Rating.class, new Rating("李雷", "男", 50), new Rating("", "女", 10), new Rating("郭靖", "男", 100));
 
         DataSet<Tuple2<String, Double>> weights = environment.fromElements(new Tuple2<>("男", 1.0), new Tuple2<>("女", 2.0));
 
@@ -49,13 +53,11 @@ public class Api {
                 .with(new JoinFunction<Rating, Tuple2<String, Double>, Tuple2<String, Double>>() {
                         @Override
                         public Tuple2<String, Double> join(Rating first, Tuple2<String, Double> second) throws Exception {
-                            return new Tuple2<>(first.name, first.points * second.f1);
+                            return new Tuple2<>(first.getName(), first.getPoints() * second.f1);
                         }
                     });
 
         tuple2DataSet.print();
-
-        ratings.print();
 
     }
 }
