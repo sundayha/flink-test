5cb33820062d4e7133aa276ed2825dd4d667f3f9
sundayha <sagapr@163.com>
2019-03-11 16:58:56 +0800

    flink 学习 测试

--- src/main/java/dataset/test/Api.java
+++ src/main/java/dataset/test/Api.java
@@ -4,9 +4,11 @@ import dataset.test.model.Rating;
 import org.apache.flink.api.common.functions.JoinFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.io.CollectionInputFormat;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.typeutils.PojoTypeInfo;
 import org.apache.flink.api.java.typeutils.TypeExtractor;
+import org.apache.flink.runtime.state.ArrayListSerializer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,7 +27,12 @@ public class Api {
         ratings.add(new Rating("李雷", "男", 50));
         ratings.add(new Rating("韩梅梅", "女", 10));
         ratings.add(new Rating("郭靖", "男", 50));
+
+        ArrayListSerializer<Rating> arrayListSerializer = new ArrayListSerializer<>();
+
+        CollectionInputFormat<Rating> collectionInputFormat = new CollectionInputFormat<>(ratings, arrayListSerializer);
         DataSet<Rating> ratingss  = environment.fromCollection(ratings, pojoTypeInfo);
+
         //environment.createInput(, pojoTypeInfo);
         //DataSet<Rating> ratings = environment.fromElements(Rating.class, new Rating("李雷", "男", 50), new Rating("", "女", 10), new Rating("郭靖", "男", 100));
 
@@ -47,6 +54,8 @@ public class Api {
                     });
 
         tuple2DataSet.print();
+
+        ratings.print();
 
     }
 }
